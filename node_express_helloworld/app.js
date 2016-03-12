8fcfc244d2ac3fb21bbd7228a1acfab905e7c8c7
tzq668766 <tzq618766@gmail.com>
2016-03-12 21:16:37 +0800

    添加一些静态文件，如js,css,图片等，并添加一个网页文件

--- node_express_helloworld/app.js
+++ node_express_helloworld/app.js
@@ -4,8 +4,35 @@
 var express=require('express');
 var app=express();
 
+//引用初始化设置:
+//静态文件目录设置
+app.use(express.static('public'));
+app.use(express.static('files'));
+
+//对网站首页的访问返回"Hello World"
 app.get('/',function(req,res){
     res.send("Hello World");
+});
+
+
+//网站首页接受POST请求
+app.post("/",function(req,res){
+    res.send("Got a POST request");
+});
+
+//PUT请求
+app.put("/user",function(req,res){
+    res.send("Got a PUT request at /user");
+});
+
+//delete请求
+app.delete("/user",function(req,res){
+    res.send("Got a Delete request at /user")
+});
+
+//发送文件
+app.get("/hello",function(req,res){
+    res.sendfile("hello.html");
 });
 
 var server=app.listen(3000,function(){
